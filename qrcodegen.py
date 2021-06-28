0ac76a7061cc50effbbae47a393dd29204c48cff
aatif4558 <imaatifali@gmail.com>
2021-06-28 15:36:39 +0530

    First Commit

---
+++ qrcodegen.py
@@ -0,0 +1,8 @@
+import pyqrcode  # pip install pyqrcode
+def qrcode_generator():
+    x=input()
+    qrcode_encrpy=pyqrcode.create(x)
+    qrcode_encrpy.png("test.png",scale=6)
+    print("Qrcode is generated successfully")
+if __name__=="__main__":
+    qrcode_generator()
